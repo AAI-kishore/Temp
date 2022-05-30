a6484ee21bda637fc7e41d957fcb41587d7dab65
kpingili <kishore.pingili@gmail.com>
2022-05-30 13:42:27 +0530

    update

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+import numpy as np
+import csv
+
+print("\n \n \n")
+print("current working directory:", os.getcwd())
+print("\n \n \n")
+print("hey there start working on your project")
+print("\n \n \n")
